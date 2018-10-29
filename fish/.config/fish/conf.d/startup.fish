1538d3e499811392aa1817496dd526136fbe636c
grsakea <grsakea@users.noreply.github.com>
2018-10-29 23:17:37 +0100

    Add tmux title updating

--- fish/.config/fish/conf.d/startup.fish
+++ fish/.config/fish/conf.d/startup.fish
@@ -1,3 +1,14 @@
 if status --is-interactive
    keychain --eval --quiet --systemd | source
 end
+
+function fish_greeting
+end
+
+function tmux_pane_title --on-event fish_prompt
+    printf "\033k"(prompt_pwd)"\033\\"
+end
+
+function tmux_exec --on-event fish_preexec
+    printf "\033k"$argv"\033\\"
+end
