0b082a29c86d06160d5e0d250947319c35df8cd7
grsakea <grsakea@users.noreply.github.com>
2018-11-19 00:07:51 +0100

    Improve tmux auto rename

--- fish/.config/fish/conf.d/startup.fish
+++ fish/.config/fish/conf.d/startup.fish
@@ -6,9 +6,13 @@ function fish_greeting
 end
 
 function tmux_pane_title --on-event fish_prompt
-    printf "\033k"(prompt_pwd)"\033\\"
+    if set -q TMUX
+        printf "\033k"(prompt_pwd)"\033\\"
+    end
 end
 
 function tmux_exec --on-event fish_preexec
-    printf "\033k"$argv"\033\\"
+    if set -q TMUX
+        printf "\033k"$argv"\033\\"
+    end
 end
