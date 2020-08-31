02c3ff6a743ba8b896e116ddf4e95f61d78342f2
jb-abbadie <jb-abbadie@users.noreply.github.com>
2020-08-31 12:48:35 +0200

    put pyenv on the left

--- dot_p10k.zsh
+++ dot_p10k.zsh
@@ -37,6 +37,7 @@
     dir
     custom_terraform
     kubecontext             # current kubernetes context (https://kubernetes.io/)
+    pyenv                   # python environment (https://github.com/pyenv/pyenv)
     terraform
     vcs
     # =========================[ Line #2 ]=========================
@@ -61,7 +62,6 @@
     asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
     virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
     anaconda                # conda environment (https://conda.io/)
-    pyenv                   # python environment (https://github.com/pyenv/pyenv)
     goenv                   # go environment (https://github.com/syndbg/goenv)
     nodenv                  # node.js version from nodenv (https://github.com/nodenv/nodenv)
     nvm                     # node.js version from nvm (https://github.com/nvm-sh/nvm)
