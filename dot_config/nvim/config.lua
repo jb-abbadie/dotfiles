f0ae43f250d177e19f6eac2f1698debf042388a4
jb-abbadie <jb-abbadie@users.noreply.github.com>
2021-07-20 07:08:07 +0200

    Move from pyls to pylsp

--- dot_config/nvim/config.lua
+++ dot_config/nvim/config.lua
@@ -66,7 +66,7 @@ local on_attach = function(client, bufnr)
   end
 end
 
-local servers = { "gopls", "rls", "terraformls", "pyls" }
+local servers = { "gopls", "rls", "terraformls", "pylsp" }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup { on_attach = on_attach }
 end
