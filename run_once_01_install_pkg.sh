5da58c7b410b3d607c827280b6328fa1ebfcde40
jb-abbadie <jb-abbadie@users.noreply.github.com>
2023-08-31 08:42:10 +0200

    Only run pacman on Arch

--- run_once_01_install_pkg.sh
+++ run_once_01_install_pkg.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
-sudo pacman -S --needed \
-    alacritty \
-    bat \
-    bemenu \
-    brightnessctl \
-    choose \
-    dog \
-    dunst \
-    fd \
-    firefox \
-    fzf \
-    hyperfine \
-    imv \
-    jq \
-    keychain \
-    kmscon \
-    kubectl \
-    kubie \
-    pavucontrol \
-    ranger \
-    ripgrep \
-    sway \
-    swayidle \
-    swaylock \
-    tmux \
-    waybar \
-    waylock \
-    zsh
+{{ if eq .chezmoi.osRelease.id "arch" }}
+ sudo pacman -S --needed \
+     alacritty \
+     bat \
+     bemenu \
+     brightnessctl \
+     choose \
+     dog \
+     dunst \
+     fd \
+     firefox \
+     fzf \
+     hyperfine \
+     imv \
+     jq \
+     keychain \
+     kmscon \
+     kubectl \
+     kubie \
+     pavucontrol \
+     ranger \
+     ripgrep \
+     sway \
+     swayidle \
+     swaylock \
+     tmux \
+     waybar \
+     waylock \
+     zsh
+{{ end }}
