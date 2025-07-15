2d3390c0d315ec12127c021c8c7ace5542bc91d1
jb-abbadie <jb-abbadie@users.noreply.github.com>
2025-07-15 16:25:12 +0200

    Migrate from antidote to sheldon

--- run_once_03_install_go_package.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-export GOPATH="$HOME/prog/go"
-
-go install github.com/kubecolor/kubecolor@latest
-go install github.com/nao1215/gup@latest
-go install github.com/fairwindsops/nova@latest
