14cd3bad10a16f78293f32f3a94ef69fad0d9b80
jb-abbadie <jb-abbadie@users.noreply.github.com>
2025-07-21 09:05:02 +0200

    Add go pack to nvim

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -5,8 +5,9 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.colorscheme.solarized-osaka-nvim" },
   { import = "astrocommunity.completion.copilot-cmp" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
 }
