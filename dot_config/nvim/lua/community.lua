87ea46bbffe45ca9fd9d2498ffc28f33595b57bb
jb-abbadie <jb-abbadie@users.noreply.github.com>
2025-12-16 10:41:03 +0100

    Cleanup mise

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -6,7 +6,7 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.colorscheme.solarized-osaka-nvim" },
-  { import = "astrocommunity.completion.copilot-cmp" },
+  -- { import = "astrocommunity.completion.copilot-cmp" },
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
