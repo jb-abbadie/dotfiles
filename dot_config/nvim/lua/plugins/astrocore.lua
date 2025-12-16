8a3e6c68efc911fa7f71335c12be8cae5e43c09c
jb-abbadie <jb-abbadie@users.noreply.github.com>
2025-12-16 15:49:14 +0100

    feat: format core.nvim

--- dot_config/nvim/lua/plugins/astrocore.lua
+++ dot_config/nvim/lua/plugins/astrocore.lua
@@ -5,92 +5,122 @@
 
 ---@type LazySpec
 return {
-  "AstroNvim/astrocore",
-  ---@type AstroCoreOpts
-  opts = {
-    -- Configure core features of AstroNvim
-    features = {
-      large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-      autopairs = true, -- enable autopairs at start
-      cmp = true, -- enable completion at start
-      diagnostics = { virtual_text = false, virtual_lines = true }, -- diagnostic settings on startup
-      highlighturl = true, -- highlight URLs at start
-      notifications = true, -- enable notifications at start
-    },
-    -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
-    diagnostics = {
-      virtual_text = true,
-      underline = true,
-    },
-    -- passed to `vim.filetype.add`
-    filetypes = {
-      -- see `:h vim.filetype.add` for usage
-      extension = {
-        foo = "fooscript",
-      },
-      filename = {
-        [".foorc"] = "fooscript",
-      },
-      pattern = {
-        [".*/etc/foo/.*"] = "fooscript",
-        [".*/.*%.yaml.gotmpl"] = "helm",
-        [".*/templates/.*%.tpl"] = "helm",
-        [".*/templates/.*%.ya?ml"] = "helm",
-        ["helmfile.*%.ya?ml"] = "helm",
-      },
-    },
-    -- vim options can be configured here
-    options = {
-      opt = { -- vim.opt.<key>
-        relativenumber = false, -- sets vim.opt.relativenumber
-        number = true, -- sets vim.opt.number
-        spell = false, -- sets vim.opt.spell
-        signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-        wrap = true, -- sets vim.opt.wrap
-        scrolloff = 5,
-      },
-      g = { -- vim.g.<key>
-        -- configure global vim variables (vim.g)
-        -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
-        -- This can be found in the `lua/lazy_setup.lua` file
-      },
-    },
-    -- Mappings can be configured through AstroCore as well.
-    -- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
-    mappings = {
-      -- first key is the mode
-      n = {
-        -- second key is the lefthand side of the map
+	"AstroNvim/astrocore",
+	---@type AstroCoreOpts
+	opts = {
+		-- Configure core features of AstroNvim
+		features = {
+			large_buf = { size = 1024 * 256, lines = 10000 },          -- set global limits for large files for disabling features like treesitter
+			autopairs = true,                                          -- enable autopairs at start
+			cmp = true,                                                -- enable completion at start
+			diagnostics = { virtual_text = false, virtual_lines = true }, -- diagnostic settings on startup
+			highlighturl = true,                                       -- highlight URLs at start
+			notifications = true,                                      -- enable notifications at start
+		},
+		-- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
+		diagnostics = {
+			virtual_text = true,
+			underline = true,
+		},
+		-- passed to `vim.filetype.add`
+		filetypes = {
+			-- see `:h vim.filetype.add` for usage
+			extension = {
+				foo = "fooscript",
+			},
+			filename = {
+				[".foorc"] = "fooscript",
+			},
+			pattern = {
+				[".*/etc/foo/.*"] = "fooscript",
+				[".*/.*%.yaml.gotmpl"] = "helm",
+				[".*/templates/.*%.tpl"] = "helm",
+				[".*/templates/.*%.ya?ml"] = "helm",
+				["helmfile.*%.ya?ml"] = "helm",
+			},
+		},
+		-- vim options can be configured here
+		options = {
+			opt = {               -- vim.opt.<key>
+				relativenumber = false, -- sets vim.opt.relativenumber
+				number = true,      -- sets vim.opt.number
+				spell = false,      -- sets vim.opt.spell
+				signcolumn = "yes", -- sets vim.opt.signcolumn to yes
+				wrap = true,        -- sets vim.opt.wrap
+				scrolloff = 5,
+			},
+			g = { -- vim.g.<key>
+				-- configure global vim variables (vim.g)
+				-- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
+				-- This can be found in the `lua/lazy_setup.lua` file
+			},
+		},
+		-- Mappings can be configured through AstroCore as well.
+		-- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
+		mappings = {
+			-- first key is the mode
+			n = {
+				-- second key is the lefthand side of the map
 
-        -- navigate buffer tabs
-        ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
-        ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
+				-- navigate buffer tabs
+				["]b"] = {
+					function()
+						require("astrocore.buffer").nav(vim.v.count1)
+					end,
+					desc = "Next buffer",
+				},
+				["[b"] = {
+					function()
+						require("astrocore.buffer").nav(-vim.v.count1)
+					end,
+					desc = "Previous buffer",
+				},
 
-        -- mappings seen under group name "Buffer"
-        ["<Leader>bd"] = {
-          function()
-            require("astroui.status.heirline").buffer_picker(
-              function(bufnr) require("astrocore.buffer").close(bufnr) end
-            )
-          end,
-          desc = "Close buffer from tabline",
-        },
+				-- mappings seen under group name "Buffer"
+				["<Leader>bd"] = {
+					function()
+						require("astroui.status.heirline").buffer_picker(function(bufnr)
+							require("astrocore.buffer").close(bufnr)
+						end)
+					end,
+					desc = "Close buffer from tabline",
+				},
 
-        ["<A-h>"] = { function() require("smart-splits").resize_left() end, desc = "Resize left" },
-        ["<A-j>"] = { function() require("smart-splits").resize_down() end, desc = "Resize down" },
-        ["<A-k>"] = { function() require("smart-splits").resize_up() end, desc = "Resize up" },
-        ["<A-l>"] = { function() require("smart-splits").resize_right() end, desc = "Resize right" },
+				["<A-h>"] = {
+					function()
+						require("smart-splits").resize_left()
+					end,
+					desc = "Resize left",
+				},
+				["<A-j>"] = {
+					function()
+						require("smart-splits").resize_down()
+					end,
+					desc = "Resize down",
+				},
+				["<A-k>"] = {
+					function()
+						require("smart-splits").resize_up()
+					end,
+					desc = "Resize up",
+				},
+				["<A-l>"] = {
+					function()
+						require("smart-splits").resize_right()
+					end,
+					desc = "Resize right",
+				},
 
-        -- tables with just a `desc` key will be registered with which-key if it's installed
-        -- this is useful for naming menus
-        -- ["<Leader>b"] = { desc = "Buffers" },
+				-- tables with just a `desc` key will be registered with which-key if it's installed
+				-- this is useful for naming menus
+				-- ["<Leader>b"] = { desc = "Buffers" },
 
-        -- setting a mapping to false will disable it
-        -- ["<C-S>"] = false,
-      },
-    },
-    rooter = {
-      autochdir = true,
-    },
-  },
+				-- setting a mapping to false will disable it
+				-- ["<C-S>"] = false,
+			},
+		},
+		rooter = {
+			autochdir = true,
+		},
+	},
 }
