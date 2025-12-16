ba06dd3d6d3cdaf1ed390fa6db5c6d35fb139a0a
jb-abbadie <jb-abbadie@users.noreply.github.com>
2025-12-16 15:52:18 +0100

    autodetect .tf as terraform

--- dot_config/nvim/lua/plugins/astrocore.lua
+++ dot_config/nvim/lua/plugins/astrocore.lua
@@ -10,12 +10,12 @@ return {
 	opts = {
 		-- Configure core features of AstroNvim
 		features = {
-			large_buf = { size = 1024 * 256, lines = 10000 },          -- set global limits for large files for disabling features like treesitter
-			autopairs = true,                                          -- enable autopairs at start
-			cmp = true,                                                -- enable completion at start
+			large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
+			autopairs = true, -- enable autopairs at start
+			cmp = true, -- enable completion at start
 			diagnostics = { virtual_text = false, virtual_lines = true }, -- diagnostic settings on startup
-			highlighturl = true,                                       -- highlight URLs at start
-			notifications = true,                                      -- enable notifications at start
+			highlighturl = true, -- highlight URLs at start
+			notifications = true, -- enable notifications at start
 		},
 		-- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
 		diagnostics = {
@@ -26,7 +26,7 @@ return {
 		filetypes = {
 			-- see `:h vim.filetype.add` for usage
 			extension = {
-				foo = "fooscript",
+				tf = "terraform",
 			},
 			filename = {
 				[".foorc"] = "fooscript",
@@ -41,12 +41,12 @@ return {
 		},
 		-- vim options can be configured here
 		options = {
-			opt = {               -- vim.opt.<key>
+			opt = { -- vim.opt.<key>
 				relativenumber = false, -- sets vim.opt.relativenumber
-				number = true,      -- sets vim.opt.number
-				spell = false,      -- sets vim.opt.spell
+				number = true, -- sets vim.opt.number
+				spell = false, -- sets vim.opt.spell
 				signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-				wrap = true,        -- sets vim.opt.wrap
+				wrap = true, -- sets vim.opt.wrap
 				scrolloff = 5,
 			},
 			g = { -- vim.g.<key>
