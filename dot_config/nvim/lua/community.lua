0f81a8a5fe01ced46a445c17ae75db2210d62e5d
jb-abbadie <jb-abbadie@users.noreply.github.com>
2025-12-23 14:25:48 +0100

    Update nvim config

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -4,10 +4,10 @@
 
 ---@type LazySpec
 return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.colorscheme.solarized-osaka-nvim" },
-  -- { import = "astrocommunity.completion.copilot-cmp" },
-  { import = "astrocommunity.pack.go" },
-  { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+	"AstroNvim/astrocommunity",
+	{ import = "astrocommunity.colorscheme.solarized-osaka-nvim" },
+	{ import = "astrocommunity.pack.go" },
+	{ import = "astrocommunity.pack.rust" },
+	{ import = "astrocommunity.pack.lua" },
+	-- import/override with your plugins folder
 }
